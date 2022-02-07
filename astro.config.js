d3fe0968eae57aca1d0fc01dc8e1c926d8530de2
Ni Ngo <ni.ngo@asnet.com.vn>
2022-02-07 09:51:41 +0700

    Add partial hydration component

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,6 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
+module.exports = {
+  projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
@@ -11,5 +11,7 @@ export default {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
-  renderers: [],
+  renderers: [
+    '@astrojs/renderer-react',
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin'),
+  ]
 };
